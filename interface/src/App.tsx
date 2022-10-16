611d0f93f2aa94d6c35f687ba2858716cc704e41
zivnadel <zywn1414@gmail.com>
2022-10-16 10:57:03 +0300

    Init

---
+++ interface/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="w-full h-full text-center">
+      <h1 className="text-red-500">Hello World</h1>
+    </div>
+  );
+};
+
+export default App;
