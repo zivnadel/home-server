c8900c2ee3df4588df6e6c723443b82e00749a59
zivnadel <zywn1414@gmail.com>
2022-10-21 16:55:56 +0300

    Server fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -42,6 +42,6 @@ app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
     .json({ message: error.message || "An unknown error occurred!" });
 });
 
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
